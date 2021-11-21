2d190161313306bc0c127dc5bf1884ab7ebcb1c4
ValyaevRasim <jsm.group@mail.ru>
2021-11-21 23:31:42 +0200

    https://howtodoinjava.com/java/cloning/a-guide-to-object-cloning-in-java/

---
+++ src/Department.java
@@ -0,0 +1,24 @@
+public class Department implements Cloneable
+{
+    private int id;
+    private String name;
+
+    public Department(int id, String name)
+    {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+}
